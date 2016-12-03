ebc58a63fca99681a1ec8cd2dcb1fc7f5c80fada
alarosa <larosa2@rpi.edu>
2016-12-02 22:22:33 -0500

    Added Camera rotate functionality

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 var gl;
 
+var d, p, r;
+
+var dragging = false;
+
+var deg_to_rad = Math.PI/180;
+var canvas;
+
 window.onload = function init()
 {
-    var canvas = document.getElementById( "gl-canvas" );
+    canvas = document.getElementById( "gl-canvas" );
 
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
@@ -40,10 +47,73 @@ window.onload = function init()
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
+    d = 0;
+    p = 10 * deg_to_rad;
+    r = 0;
+
+    var rMatrixLoc = gl.getUniformLocation( program, "rMatrix" );
+
+    canvas.addEventListener("mousedown", doMouseDown, false);
+
+    sendRotationMatrix(rMatrixLoc);
     render(vertices.length);
+
+
+    /* Event Declarations */
+    function doMouseDown(evt) {
+        if (dragging)
+           return;
+        dragging = true;
+        document.addEventListener("mousemove", doMouseDrag, false);
+        document.addEventListener("mouseup", doMouseUp, false);
+        var box = canvas.getBoundingClientRect();
+        prevx = window.pageXOffset + evt.clientX - box.left;
+        prevy = window.pageYOffset + evt.clientY - box.top;
+
+    }
+    function doMouseDrag(evt) {
+        if (!dragging)
+           return;
+        var box = canvas.getBoundingClientRect();
+        var x = window.pageXOffset + evt.clientX - box.left;
+        var y = window.pageYOffset + evt.clientY - box.top;
+
+        d += (((x- prevx) * -1) * deg_to_rad);
+        p += (((y- prevy) * 1) * deg_to_rad);
+
+        p = Math.max(10*deg_to_rad, Math.min(p, 40* deg_to_rad))
+
+
+        prevx = x;
+        prevy = y;
+        if (render) {
+            sendRotationMatrix(rMatrixLoc);
+            render(vertices.length);
+        }
+    }
+    function doMouseUp(evt) {
+        if (dragging) {
+            document.removeEventListener("mousemove", doMouseDrag, false);
+            document.removeEventListener("mouseup", doMouseUp, false);
+        dragging = false;
+        }
+        sendRotationMatrix(rMatrixLoc);
+        render(vertices.length);
+    }
 };
+
+function sendRotationMatrix(rMatrixLoc) {
+
+    var r_matrix = [ Math.cos(d)*Math.cos(r),Math.cos(d)*Math.sin(r), -1*Math.sin(d),0,
+                    (Math.sin(p)*Math.sin(d)*Math.cos(r) - Math.cos(p)*Math.sin(r)), (Math.sin(p)*Math.sin(d)*Math.sin(r) + Math.cos(p)*Math.cos(r)), Math.sin(p)*Math.cos(d), 0,
+                    (Math.cos(p)*Math.sin(d)*Math.cos(r) + Math.sin(p)*Math.sin(r)), (Math.cos(p)*Math.sin(d)*Math.sin(r) - Math.sin(p)*Math.cos(r)), Math.cos(p)*Math.cos(d), 0,
+                    0,0,0,1];
+
+    gl.uniformMatrix4fv(rMatrixLoc, false, r_matrix);
+}
 
 function render(size) {
     gl.clear( gl.COLOR_BUFFER_BIT );
     gl.drawArrays( gl.TRIANGLES, 0, size);
+
 }
